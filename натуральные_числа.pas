c5804a9bc912bc8379c4226bab4163c453e5125d
CarolineFs <dr_gromova1998@mail.ru>
2016-11-26 22:09:21 +0300

    Update натуральные_числа.pas

--- натуральные_числа.pas
+++ натуральные_числа.pas
@@ -5,6 +5,6 @@ begin
 writeln ('Введите натуральное число');
 readln (N);
 for b:=1 to N do
-    if b mod 2=0 (* or b mod 3=  0  or b mod 5= 0*) then continue
+    if (b mod 2=0) or (b mod 3=0)  or (b mod 5= 0) then continue
     else writeln (b);
 end. 

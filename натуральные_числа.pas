d00d0ea2dc9996ab2543dc42c393e41b24a8cfa8
CarolineFs <dr_gromova1998@mail.ru>
2016-11-26 17:33:03 +0300

    Create натуральные_числа.pas

---
+++ натуральные_числа.pas
@@ -0,0 +1,10 @@
+Program i;
+var 
+N, b: integer;
+begin
+writeln ('Введите натуральное число');
+readln (N);
+for b:=1 to N do
+    if b mod 2=0 (* or b mod 3=  0  or b mod 5= 0*) then continue
+    else writeln (b);
+end. 

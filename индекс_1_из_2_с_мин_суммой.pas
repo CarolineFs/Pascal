ebf4bcedb3665b5efd594626e72009dcd4f38273
CarolineFs <dr_gromova1998@mail.ru>
2016-12-03 20:34:20 +0300

    Create индекс_1_из_2_с_мин_суммой.pas

---
+++ индекс_1_из_2_с_мин_суммой.pas
@@ -0,0 +1,21 @@
+program X;
+const N = 10;
+var a: array [1..N] of integer;
+i, p, s, sum: integer;
+begin
+i:=1;
+p:=1;
+readln (a[i]);
+i:=2;
+readln (a[i]);
+s:=a[1]+a[2];
+for i:=3 to N do begin
+    readln (a[i]);
+    sum:=a[i-1]+a[i];
+    if sum<s then begin
+        s:=sum;
+        p:=i-1;
+        end;
+    end;
+writeln ('Индекс: ', p);
+end.

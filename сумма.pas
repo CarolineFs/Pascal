6b6e237d2ce1934c882d7ce84e0b1991145e833d
CarolineFs <dr_gromova1998@mail.ru>
2016-12-23 21:20:06 +0300

    Create сумма.pas
    Ввести целое положительное двузначное число. Найти сумму его цифр. Выполнить проверку правильности ввода информации.

---
+++ сумма.pas
@@ -0,0 +1,11 @@
+Program u;
+var a,s: integer;
+begin
+writeln ('Введите целое положительное двузначное число: ');
+readln (a);
+if (a>=9) and (a<=99) then begin
+    s:=(a mod 10)+(a div 10);
+    writeln (s);
+    end
+    else writeln ('Это не целое положительное двузначное число!');
+end. 

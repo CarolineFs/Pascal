025e26d6c80d98733c7bd617a4fd44916083d833
CarolineFs <dr_gromova1998@mail.ru>
2016-12-23 22:01:29 +0300

    Update сумма.pas

--- сумма.pas
+++ сумма.pas
@@ -3,7 +3,7 @@ var a,s: integer;
 begin
 writeln ('Введите целое положительное двузначное число: ');
 readln (a);
-if (a>=9) and (a<=99) then begin
+if (a>=10) and (a<=99) then begin
     s:=(a mod 10)+(a div 10);
     writeln (s);
     end

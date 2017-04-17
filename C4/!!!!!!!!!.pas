61af51f575dec272db69c2a48556a3052d8f892f
CarolineFs <dr_gromova1998@mail.ru>
2017-04-17 23:17:58 +0300

    Create !!!!!!!!!.pas

---
+++ C4/!!!!!!!!!.pas
@@ -0,0 +1,48 @@
+program roma;
+const
+    M = 100;
+var
+    score: array [1..M] of integer;
+    i, k, x, N, av, j: integer;
+    c: char;
+begin
+    writeln ('Number: ');
+    readln (N);
+    k := 0;
+    
+    for i:=1 to N do
+        score[i] := 0;
+    for i:=1 to N do
+        begin
+            while k <> 3 do
+                begin
+                    read(c);
+                    if c = ' ' then k += 1;
+                end;
+            k := 0;
+            readln(x);
+            score[x] := score[x] + 1;
+        end;
+    
+    av := Trunc(N*0.2);
+    for i := 100 to 1 do 
+      if score[i] >= av then 
+        begin 
+          writeln (i);
+          break; 
+        end
+      else
+        begin
+            k := 0;
+            for j := 100 to 1 do
+              begin
+                k := k + score[i];
+                if k >= av then
+                  begin
+                    writeln(i);
+                    break;
+                  end;
+              end;
+        end;     
+                
+    end.

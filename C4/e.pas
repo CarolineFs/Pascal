0a19460e8a4961bea7ff8d630ff20b46679bd780
CarolineFs <dr_gromova1998@mail.ru>
2017-05-17 19:24:57 +0300

    Update e.pas

--- C4/e.pas
+++ C4/e.pas
@@ -1,16 +1,26 @@
+(* Программа вычисляет количество значительных подъемов. 
+Подъем значительный, если высота подъема больше величины минимального (первого) элемента этого подъема *)
 var
-   i, k, x, y, min, kp: integer;
+   k, x, y, min, kp: integer;
 begin
   readln(x);
-  y:=x;
   min:=x;
   k:=1;
+  kp:=0;
     while x<>0 do
        begin
+         y:=x;
          readln(x);
-         k:=k+1;
-         if x<y then
-           begin
-             if x-y>min then kp:=kp+1;
-             min:=x;
-             y:=x;
+         
+         if x<>0 then k:=k+1;
+         if x<y then 
+          begin
+           if y-min>min then kp:=kp+1;
+            min:=x;
+          end;
+            
+          
+       end; 
+  writeln ('Получено чисел: ', k);
+  writeln ('Найдено значительных подъемов: ', kp);
+end. 
